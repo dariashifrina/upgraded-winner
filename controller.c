3b71589dc1c6bdc956f90ed032da976fe697b27b
dariashifrina <dshifrina@stuy.edu>
2017-12-09 15:11:07 -0500

    finished controller file

--- controller.c
+++ controller.c
@@ -1,70 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/sem.h>
 #include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <unistd.h>
 #include <string.h>
-#include <errno.h>
+#include <fcntl.h>
 
 #define KEY 12
 
-void createsem(char *argv[]){
-    int id = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0600);
-    if(id < 0){
+//doesnt work without manual declaration for some reason. say storage size su unknown
+union semun {
+  int val;
+  struct semid_ds *buf;
+  unsigned short int *array;
+  struct seminfo *__buf;
+};
+
+void createsem(){
+  int shmid = shmget(KEY, 1024, IPC_CREAT | IPC_EXCL | 0644); //shared mem
+    if(shmid < 0){
       printf("Semaphore already exists, please remove using ./a.out -r before tryin again.\n");
     }
     else{
-      int newsem = semctl(id,0,SETVAL,atoi(argv[2]));
-      printf("Semaphore created! Here it is: %d\n", id);
+      int newsem = open("story.txt", O_CREAT | O_TRUNC, 0644); //creating story file
+      int semid = semget(KEY, 1, IPC_CREAT | IPC_EXCL | 0644); //getting sem id
+      printf("Semaphore created! Here it is: %d\n", semid);
+      union semun su;
+      su.val = 1;
+      semctl(semid,0,SETVAL, su); //storing metadata
     }
 }
 
-int viewsem(char *argv[]){
-  int id = semget(KEY, 1, 0600);
-  int semneeded = semctl(id,0,GETVAL);
-  if(semneeded == -1){
+void viewsem(){ //view current story
+  int shmid = shmget(KEY, 1024, 0644); //shared mem
+  // int semneeded = semctl(id,0,GETVAL);
+  if(shmid == -1){
     printf("Semaphore doesn't exist. Please use ./a.out -c before attempting to view the file.\n");
-    return 0;
   }
   else{
-    printf("Semaphore %d's value: %d\n", id, semneeded);
-    return 1;
+    char * story = (char *)calloc(sizeof(char),1024);
+    int fd = open("story.txt",O_RDONLY, 0644);
+    read(fd,story,1024);
+    printf("Story so far: %s\n",story);
   }
 }
 
-void removesem(char *argv[]){
-  int id = semget(KEY, 1, 0600);
-  int semneeded = semctl(id,0,IPC_RMID);
-  if(semneeded == -1){
+void removesem(){
+  int shmid = shmget(KEY, 1024, 0); //shared mem
+  //int semneeded = semctl(id,0,IPC_RMID);
+  if(shmid == -1){
     printf("Semaphore doesn't exist. Please use ./a.out -c before attempting to remove the file.\n");
   }
   else{
-    printf("Semaphore is removed: %d\n", semneeded);
+    int semid = semget(KEY, 1, 0);
+    char * story = (char *)calloc(sizeof(char),1024);
+    int fd = open("story.txt",O_RDONLY, 0644);
+    read(fd,story,1024);
+    printf("Story is being removed: %s\n",story); 
+    shmctl(shmid,IPC_RMID,0); //marking segment to be destroyed and destroys it
+    semctl(semid,0,IPC_RMID,0); //removing semaphore indetifier and freeing storage
+    remove("story.txt"); //story removed
   }
 }
 
 
 ///////////////////////////////////////////////////////////////
 
-/*int main(int argc, char *argv[]){
+int main(int argc, char *argv[]){
   int id;
   if(argc < 2){
     printf("USE ONLY ONE ARGUMENT.\n");
   }
     else{
       if(!strcmp("-c", argv[1])){
-	createsem(argv);
+	createsem();
     }
     else if(!strcmp("-v", argv[1])){
-      viewsem(argv);
+      viewsem();
     }
       
     else if(!strcmp("-r", argv[1])){
-      removesem(argv);
+      removesem();
     }
     }
   return 0;
 }
-*/
+
